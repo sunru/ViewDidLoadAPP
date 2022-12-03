32b1b7979b962e6aaa6b323d007a0092c8ea7393
RubySun <sunru10075@gmail.com>
2022-12-03 18:33:37 +0800

    Initial Commit

---
+++ ViewDidLoadAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewDidLoadAPP
+//
+//  Created by 廖晨如 on 2022/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
